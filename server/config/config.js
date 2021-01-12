fad693376d8eeba1d5076393b1f1743c3cc3c638
christian-rojas <christian.ici17@gmail.com>
2021-01-12 07:11:24 -0300

    variable de entorno

--- server/config/config.js
+++ server/config/config.js
@@ -11,7 +11,7 @@ let urlDB;
 if(process.env.NODE_ENV === 'dev'){
     urlDB = 'mongodb://localhost:27017/local';
 }else{
-    urlDB = 'mongodb+srv://cook:meister10@cluster0.cjvht.mongodb.net/restserver';
+    urlDB = process.env.MONGO_URL;
 }
 
 process.env.URLDB = urlDB;
